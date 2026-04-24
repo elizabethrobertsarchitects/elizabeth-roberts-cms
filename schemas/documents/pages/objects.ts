c613284b533470c35e2acec9efc30a4a9a7a8e81
Sue Roh <sue_roh@newyorker.com>
2026-04-24 15:04:22 -0400

    feat: switched objects gallery page to use same preview format as projects gallery

--- schemas/documents/pages/objects.ts
+++ schemas/documents/pages/objects.ts
@@ -1,49 +1,57 @@
 const objectPage = {
-    name: 'objectPage',
-    type: 'object',
-    fields: [
+  name: 'objectPage',
+  type: 'object',
+  fields: [
+    {
+      type: 'array',
+      name: 'headerGallery',
+      of: [
         {
-            type: 'array',
-            name: 'headerGallery',
-            of: [
-                {
-                    name: 'featuredObject',
-                    type: 'object',
-                    fields: [
-                        {
-                            name: 'objectReference',
-                            type: 'reference',
-                            to: [{ type: 'objectItem' }],
-                        },
-                        {
-                            name: 'alternateImage',
-                            type: 'customImage'
-                        }
-                    ],
-                    preview: {
-                        select: {
-                            title: 'objectReference.title',
-                            mainImage: 'objectReference.mainImage',
-                            alternateImage: 'alternateImage'
-                        },
-                        prepare (data: any) {
-                            return {
-                                title: data.title ? data.title : 'Object',
-                                media: data.alternateImage?.image ? data.alternateImage.image : data.mainImage?.image
-                            }
-                        }
-                    }
-                }
-            ]
-        }
-    ],
-    preview: {
-        prepare: () => {
-            return {
-                title: 'Main Page'
-            }
-        }
-    }
+          name: 'featuredObject',
+          type: 'object',
+          fields: [
+            {
+              name: 'objectReference',
+              type: 'reference',
+              to: [{type: 'objectItem'}],
+            },
+            {
+              name: 'alternateImage',
+              type: 'customImage',
+            },
+          ],
+          preview: {
+            select: {
+              title: 'objectReference.title',
+              mainImage: 'objectReference.mainImage',
+              alternateImage: 'alternateImage',
+            },
+            prepare(data: any) {
+              return {
+                title: data.title ? data.title : 'Object',
+                media: data.alternateImage?.image
+                  ? data.alternateImage.image
+                  : data.mainImage?.image,
+              }
+            },
+          },
+        },
+      ],
+    },
+    {
+      name: 'numberOfPreviewImages',
+      description:
+        'Number of preview images that cycle through on hover and mouse move on /objects. Default value is 3.',
+      type: 'number',
+    },
+  ],
+  preview: {
+    prepare: () => {
+      return {
+        title: 'Main Page',
+      }
+    },
+  },
 }
 
 export default objectPage
